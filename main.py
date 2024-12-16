60e6e45be76f5da4da04dd5bc852c0aaf2f9a220
Harshith <gadeHarshithReddy@gmail.com>
2024-12-15 18:16:55 -0600

    I did a bit of the show_schedule function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 
 # default settings
 starting_work_time = None
@@ -17,34 +18,42 @@ def validator(prompt, ):
 def add_work(group=None, name=None, time_for_work=None):
     # Takes input from user if not already provided
     if group is None:
-
-        # Shows the options to choose a group
-        work_group_number = 0
-        for key, value in work_groups.items():
-            work_group_number += 1
-            print(f"{work_group_number}. {key}")
-            work_number = 0
-            if len(value) > 1:
-                for work, time in value[1].items():
-                    work_number += 1
-                    print(f"    {work_number}. {work}, Time: {time} minutes")
-        print(f"{work_group_number+1}. This option adds a new group.")
-
-        # Asks user to choose from list of groups
-        group_number = int(input("What work group would you like to add it to? (type in # that corresponds): "))
-
-        # Checks if the user choose the option of adding a new group
-        if group_number != work_group_number + 1:
-            # Takes that number and finds the name of the group
+        if work_groups != {}:
+            # Shows the options to choose a group
             work_group_number = 0
-            for key in work_groups:
+            for key, value in work_groups.items():
                 work_group_number += 1
-                if work_group_number == group_number:
-                    group = key
-        # Allows the user to add a new group and a work for the group
+                print(f"{work_group_number}. {key}")
+                work_number = 0
+                if len(value) > 1:
+                    for work, time in value[1].items():
+                        work_number += 1
+                        print(f"    {work_number}. {work}, Time: {time} minutes")
+            print(f"{work_group_number+1}. This option adds a new group. Then you can add a work to this group.")
+
+            # Asks user to choose from list of groups
+            group_number = int(input("What work group would you like to add your work to? "
+                                     "(type in # that corresponds): "))
+
+            # Checks if the user choose the option of adding a new group
+            if group_number != work_group_number + 1:
+                # Takes that number and finds the name of the group
+                work_group_number = 0
+                for key in work_groups:
+                    work_group_number += 1
+                    if work_group_number == group_number:
+                        group = key
+            # Allows the user to add a new group and a work for the group
+            else:
+                add_group()
+                add_work()
+                return
         else:
+            print("No work groups detected.")
+            print("You will need to add a work group first.")
             add_group()
             add_work()
+            return
 
     if name is None:
         name = input("Please enter the name of this work: ")
@@ -52,7 +61,7 @@ def add_work(group=None, name=None, time_for_work=None):
     if time_for_work is None:
         time_for_work = int(input("About how many minutes that does this work take? "))
 
-    #
+    # Checks if a provided group is not in work_groups
     if group not in work_groups:
         group_response = input("Group not found. Would you like to add a group called " + group + " ? (y/n) ").lower()
         while group_response != "y" and group_response != "n":
@@ -65,6 +74,7 @@ def add_work(group=None, name=None, time_for_work=None):
     # Add the work to the specific group
     else:
         work_groups[group].append({name: time_for_work})
+        print(f"Work '{name}' has been added to the work group '{group}'.")
 
 
 def add_group(group_name=None, group_priority=None):
@@ -88,8 +98,38 @@ def add_group(group_name=None, group_priority=None):
     # Adds the work group, if it already doesn't exist
     if group_name not in work_groups:
         work_groups[group_name] = [group_priority]
+        print(f"Work Group '{group_name}' has been added.")
     else:
         print("Work group already exists!")
+
+
+def show_schedule():
+    print("Here is your schedule for today.")
+
+    # TODO ask Arnav about error, original work_groups dictionary items being deleted
+    # Creates a list of works in order of priority
+    priority_ordered_work = []
+
+    # A copy list made to order works by priority level
+    copy_work_groups = work_groups
+
+    for i in range(len(work_groups)):
+        priority = 0
+        priority_works = None
+        priority_work_group = None
+        for key, value in copy_work_groups.items():
+            if priority < value[0]:
+                priority = value[0]
+                priority_works = value[1]
+                priority_work_group = key
+        priority_ordered_work.append(priority_works)
+        del copy_work_groups[priority_work_group]
+
+    print(priority_ordered_work)
+    """
+    time = datetime.now()
+    hour_and_minutes = [time.hour, time.minute]
+    """
 
 
 def print_commands():
@@ -121,6 +161,8 @@ def check_user_input(user_input):
         add_group()
     elif user_input == "a":
         add_work()
+    elif user_input == "s":
+        show_schedule()
 
 
 while True:
